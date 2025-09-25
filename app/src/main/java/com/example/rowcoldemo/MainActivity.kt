99beb8ed93edb085d314afdb4f50de157c09b1e1
Aleksandr <aleksandrmurzin1@mail.ru>
2025-09-25 17:00:14 +0700

    Расстояние между элементами макета

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -49,8 +49,8 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(horizontalArrangement = Arrangement.End,
-        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier.width(1000.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")
